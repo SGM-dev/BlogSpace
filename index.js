e2e3004ca93861ed505b9d3199bbd2c1b52707ed
Sam George Mathew <37585951+SGM-dev@users.noreply.github.com>
2023-08-12 09:09:45 +0530

    display blogs on page

--- index.js
+++ index.js
@@ -2,5 +2,14 @@ fetch("https://apis.scrimba.com/jsonplaceholder/posts")
   .then((response) => response.json())
   .then((data) => {
     const postArr = data.slice(0, 5);
-    console.log(postArr);
+    let html = ``;
+    postArr.forEach((element) => {
+      html += `
+        <div class="post">
+          <h3 class="post-title">${element.title}</h4>
+          <p class="post-content">${element.body}</sp>
+        </div>
+        <hr />`;
+    });
+    document.getElementById("blog-section").innerHTML += html;
   });
